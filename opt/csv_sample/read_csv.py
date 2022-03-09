6d1a5cef286bb92b58436f612da04c030f5b9f38
murata0531 <coless115@gmail.com>
2022-03-09 23:43:55 +0900

    関数化して処理の流れをわかりやすくした

--- opt/csv_sample/read_csv.py
+++ opt/csv_sample/read_csv.py
@@ -1,19 +1,25 @@
 import numpy as np
 import pandas as pd
-data = pd.read_csv('sample.csv')
-# 使用する列を抽出する
-x = data[['値']]
-# リストに変換
-x = x.to_numpy().tolist()
-# 表示して確認する
-print(x)
-# 合計
-print(np.sum(x))
-# 平均
-print(np.mean(x))
-# 最大値を取得
-print(np.max(x))
-# 最大値のインデックスを取得して都道府県名を取得
-argmax = np.argmax(x)
-prefecture = data[['都道府県']].to_numpy().tolist()
-print(prefecture[argmax])
+
+# メイン関数
+def main():
+    data = pd.read_csv('sample.csv')
+    # 使用する列を抽出する
+    x = data[['値']]
+    # リストに変換
+    x = x.to_numpy().tolist()
+    # 表示して確認する
+    print(x)
+    # 合計
+    print(np.sum(x))
+    # 平均
+    print(np.mean(x))
+    # 最大値を取得
+    print(np.max(x))
+    # 最大値のインデックスを取得して都道府県名を取得
+    argmax = np.argmax(x)
+    prefecture = data[['都道府県']].to_numpy().tolist()
+    print(prefecture[argmax])
+
+if __name__ == "__main__":
+    main()
