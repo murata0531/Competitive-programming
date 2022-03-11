fc8cadfaa9e23160dfe6b402ea10f147ff43c16b
murata0531 <coless115@gmail.com>
2022-03-12 00:04:27 +0900

    行方向の集計を行う関数を追加

--- opt/csv_sample/read_csv.py
+++ opt/csv_sample/read_csv.py
@@ -22,6 +22,20 @@ def get_max_prefecture(data):
     prefecture = data[['都道府県']].to_numpy().tolist()
     print(prefecture[argmax])
 
+# 行方向の集計を行う関数
+def get_row_aggregate(data):
+
+    # 使用する列を抽出する
+    x = data[['値1','値2']]
+    # リストに変換
+    x = x.to_numpy().tolist()
+    # 表示して確認する
+    print(x)
+    # 行ごとの合計
+    print(np.sum(x,axis=1))
+    # 行ごとの平均
+    print(np.mean(x,axis=1))
+
 # メイン関数
 def main():
 
@@ -30,6 +44,8 @@ def main():
     data = pd.read_csv(args[1])
     # 最大値をもつ都道府県を出力する関数を呼び出す
     get_max_prefecture(data)
+    # 行方向の集計を行う関数を呼び出す
+    get_row_aggregate(data)
 
 if __name__ == "__main__":
     main()
